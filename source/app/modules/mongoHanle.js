4c236b4578a891c3384af0ce6e0d0ab10722b7f7
neekey <ni184775761@gmail.com>
2011-10-07 00:08:07 +0800

    1、添加部分schema method 对数据操作进行逻辑封装（添加中….）

--- source/app/modules/mongoHanle.js
+++ source/app/modules/mongoHanle.js
@@ -1,22 +1,55 @@
 var Mgo = require( './mongoModel' );
 
-/*
-var Muser = Mgo.model( 'user' ),
-	user =  new Muser({
-		name: 'neekey',
-		password: 'password'
-	});
+handle = {
 
-user.tags.push({ value: 'study' });
+	/** 
+	 * 添加用户
+	 */
+	addUser: function( name, password, next ){
+	},
 
-user.save(function( err ){
+	/**
+	 * 添加笔记
+	 */
+	addNote: function( username, note, next ){
+	},
+
+	/**
+	 * 删除笔记
+	 */
+	delNote: function( username, id, next ){
+	}
+
+};
+var Muser = Mgo.model( 'user' );
+
+var user = new Muser({
+	name: String( Date.now() ),
+	password: 'test'
+});
+user.tags.push({ value: String( Date.now() )});
+user.addNote({
+	content: String( Date.now() ),
+	tags: [ 'drinking' ]
+});
+
+console.log( 'user added!' );
+console.log( user );
+
+console.log( 'del note' );
+user.delNote( user.notes[ 0 ]._id );
+
+console.log( 'note deleted!' );
+console.log( user.tags );
+console.log( user );
+
+user.save( function( err ){
 	if( err ){
 		console.log( err );
 	}
 	else {
-		console.log( 'save success' );
+		console.log( 'save success!' );
 	}
 });
-*/
 
 module.exports = Mgo;
--- source/app/modules/mongoModel.js
+++ source/app/modules/mongoModel.js
@@ -3,27 +3,182 @@
  * @Author neekey<ni184775761@gmail.com>
  */
 var mongoose = require('./mongoConf'),
+	_ = require('underscore'),
 	schema = mongoose.Schema;
 
+/* define schema */
+
+// note
 var Snote = new schema({
-	id: { type: Number, unique: true },
-	content: { type: String },
+	content: { type: String, required: true },
 	created: { type: Date, 'default': Date.now },
 	updated: { type: Date, 'default': Date.now },
 	tags: [ String ]
 }),
 
+// tag
 Stag = new schema({
 	value: { type: String, required: true, unique: true },
 	notes: [ String ]
 }),
 
+// user
 Suser = new schema({
-	name: { type: String, unique: true },
-	password: { type: String },
+	name: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
 	notes: [ Snote ],
 	tags: [ Stag ] 
 });
+
+/* define schema method */
+
+// add note
+Suser.methods.addNote = function( note ){
+	if( !note.content ){
+		return false;
+	}
+	else {
+		var newNote = { content: note.content },
+			that = this;
+
+		if( note.tags ){
+			newNote.tags = note.tags; 
+		}
+
+		this.notes.push( newNote );
+
+		// this is the added note with _id
+		newNote =  _.last( this.notes );
+
+		// add note record to tags
+		if( note.tags ){
+			_.each( note.tags, function( tag ){
+				that.addTagNote( tag, newNote._id );
+			});
+		}
+		return true;
+	}
+};
+
+// update note
+Suser.methods.updateNote = function( id, update ){
+	var note = this.notes.id( id ), tags, i;
+	if( note ){
+		if( update.content ){
+			note.content = update.content;
+		}
+		if( update.tags ){
+			note.tags = update.tags;
+		}
+		return true;
+	}
+	return false;
+};
+
+// delete note
+Suser.methods.delNote = function( id ){
+	var note = this.notes.id( id ), 
+		that = this, tags;
+
+	if( note ){
+		tags = note.tags;
+		note.remove();
+
+		// remove records from tags
+		_.each( tags, function( tag ){
+			that.delTagNote( tag, id );
+		});
+		return true;
+	}
+	return false;
+};
+
+// get tag by tagName
+Suser.methods.getTagByName = function( name ){
+	var tag = _.select( this.tags, function( tag ){
+		return tag.value === name;
+	});
+
+	if( tag[ 0 ] ){
+		return tag[ 0 ];
+	}
+	return false;
+};
+
+// add tag
+Suser.methods.addTag = function( tag ){
+	if( _.indexOf( this.tags, tag ) >= 0 ){
+		return false;
+	}
+	else {
+		this.tags.push({ value: tag });
+		return true;
+	}
+};
+
+// delete tag
+Suser.methods.delTagById = function( id ){
+	var tag = this.tags.id( id );
+	if( tag ){
+		tag.remove();
+		return true;
+	}
+	return false;
+};
+
+Suser.methods.delTagByName = function( t ){
+	var tag = this.getTagByName( t );
+	if( tag !== false ){
+		tags.remove();
+		return true;
+	}
+	return false;
+};
+
+// add tag-note
+Suser.methods.addTagNote = function( t, id ){
+	var tag = this.getTagByName( t ),
+		note = this.notes.id( id );
+
+	// if note is not exist
+	if( !note ){
+		return false;
+	}
+	// if tag is not exist, add one
+	if( tag === false ){
+
+		this.addTag( t );
+		tag = this.getTagByName( t );
+	}
+	// if id is already in tag.notes
+	if( _.indexOf( tag.notes, String( id ) ) >= 0 ){
+		return false;
+	}
+
+	tag.notes.push( id );
+
+	console.log( this.tags );
+	return true;
+};
+
+// del tag-note
+Suser.methods.delTagNote = function( t, id ){
+	var tag = this.getTagByName( t ), noteIndex;
+	console.log( 'del -tag-note');
+	console.log( tag );
+	if( tag !== false ){
+		noteIndex = _.indexOf( tag.notes, String( id ) );
+		console.log( 'noteIndex' );
+		console.log( noteIndex );
+		if( noteIndex >= 0 ){
+			tag.notes.splice( noteIndex, 1 );
+			return true;
+		}
+		return false;
+	}
+	return false;
+};
+
 
 mongoose.model( 'user', Suser );
 
