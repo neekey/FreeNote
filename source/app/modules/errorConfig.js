e9063d083636a1e5d4144a99e72abe39a13aaac5
neekey <ni184775761@gmail.com>
2011-10-12 15:35:05 +0800

    添加错误信息配置

---
+++ source/app/modules/errorConfig.js
@@ -0,0 +1,19 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: neekey
+ * Date: 11-10-12
+ * Time: обнГ2:35
+ *
+ * error tip configuration
+ */
+
+var error = {
+
+    // mongo error
+    MONGO: 'database query error',
+
+    // user
+    USER_NOT_EXIST: 'username: <%= name %> not exist',
+
+    //
+}
