80c7928f2502a6dc9bd3667aac97e52b082a28ff
neekey <ni184775761@gmail.com>
2011-10-30 22:29:01 +0800

    update

--- source/app/assets/js/views/noteItem.js
+++ source/app/assets/js/views/noteItem.js
@@ -9,6 +9,7 @@ var MODELS = APP.models,
     SCREEN = MODS.screen,
     VIEWS = APP.views,
     TOUCH = MODS.touch,
+    TRANS = MODS.transform,
     TPL = MODS.tpl;
 
 var Vnote = Backbone.View.extend({
@@ -26,13 +27,26 @@ var Vnote = Backbone.View.extend({
         this.content = this.$( '.content' );
         this.tags = this.$( '.tag' );
         this.noteList = $( '#J_note-list' );
+
+        this.position();
+        // 第一次渲染
         this.render();
 
-        this.model.bind( 'change', this.render, this );
+        this.model.bind( 'change:content', this.render, this );
+        this.model.bind( 'change:tags', this.render, this );
+        this.model.bind( 'change:x', this.position, this );
+        this.model.bind( 'change:y', this.position, this );
+        this.bind( 'move', this.render, this );
 
-        TOUCH.dbClick( this.el[ 0 ], this._noteTouch, this );
+        // 添加双击触发
+        TOUCH.dbClick( this.el[ 0 ], function(){
+            this.trigger( 'noteTouch' );
+        }, this );
+
+        // 添加拖拽
         TOUCH.drag( this.el[ 0 ], this.el[ 0 ] );
 
+        // 加入到dom树中
         this.el.appendTo( this.noteList );
     },
 
@@ -40,6 +54,10 @@ var Vnote = Backbone.View.extend({
 
     },
 
+    /**
+     * 更具model更新content和tags
+     * 更具view当前位置更新model（silent）
+     */
     render: function(){
 
         var model = this.model,
@@ -48,11 +66,27 @@ var Vnote = Backbone.View.extend({
 
         this.content.text( content );
         this.tags.text( tag );
+
+        // 根据当前translate来更新model
+        var trans = TRANS.get( this.el[ 0 ], 'translate' );
+
+        this.model.set( trans, {
+            silent: true
+        });
     },
 
-    _noteTouch: function(){
+    /**
+     * 根据当前model的值来对view进行定位
+     */
+    position: function(){
 
-        this.trigger( 'noteTouch' );
+        var x = this.model.get( 'x' ),
+            y = this.model.get( 'y' );
+
+        TRANS.set( this.el[ 0 ], 'translate', {
+            x: x,
+            y: y
+        });
     },
 
     edit: function(){
