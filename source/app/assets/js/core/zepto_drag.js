6f32e46783a32b4b4ad96819089a8e159d94e35a
neekey <ni184775761@gmail.com>
2011-11-01 11:07:41 +0800

    优化性能，但是这样的话就无法支持多点同时拖拽（实际需求也不需要）

--- source/app/assets/js/core/zepto_drag.js
+++ source/app/assets/js/core/zepto_drag.js
@@ -1,6 +1,6 @@
-//     Zepto.js
-//     (c) 2010, 2011 Thomas Fuchs
-//     Zepto.js may be freely distributed under the MIT license.
+/**
+ * drag for Zepto.js
+ */
 
 (function($){
 
@@ -30,90 +30,90 @@
             dragDir = 'xy';
         }
 
-        var that = this;
+        var that = this,
+            startX, startY, lastX, lastY,
+            curX = 0, curY = 0;
 
         _.each( handlers, function( handler ){
-
-            var startX, startY, lastX, lastY,
-                curX = 0, curY = 0;
 
             $( handler ).bind( 'touchstart', dragStart, false );
             $( handler ).bind( 'touchmove', dragMove, false );
             $( handler ).bind( 'touchend', dragEnd, false );
-
-            function dragStart( e ) {
-
-                e.preventDefault();
-
-                if ( ! e.touches.length ) return;
-
-                var touch = e.touches[0],
-                    webkitTransform = that[ 0 ].style.webkitTransform,
-                    lastArr = translateEx.exec( webkitTransform );
-
-                if( lastArr ){
-                    lastArr = lastArr[ 1 ].split( ',' );
-                }
-                else {
-                    lastArr = [ 0, 0 ];
-                }
-
-                startX = touch.pageX;
-                startY = touch.pageY;
-
-                lastX = parseInt( lastArr[ 0 ] );
-                lastY = parseInt( lastArr[ 1 ] );
-
-                that.trigger( 'dragStart' );
-            }
-
-            function dragMove( e ){
-
-                e.preventDefault();
-
-                if ( !e.touches.length) return;
-
-                var touch = e.touches[0];
-                    webkitTransform = that[ 0 ].style.webkitTransform,
-                    translateStr = 'translate(';
-
-                curX = touch.pageX - startX + lastX;
-                curY = touch.pageY - startY + lastY;
-
-                if( dragDir === 'xy' ){
-
-                    translateStr += curX + 'px, ' + curY + 'px)';
-                }
-                else if( dragDir === 'x' ){
-
-                    translateStr += curX + 'px, 0px )';
-                }
-                else {
-
-                    translateStr += '0px, ' + curY + 'px)';
-                }
-
-                if( translateEx.test( webkitTransform ) ){
-
-                    that[ 0 ].style.webkitTransform = webkitTransform.replace( translateEx, translateStr );
-                }
-                else {
-
-                    that[ 0 ].style.webkitTransform += translateStr;
-                }
-
-                that.trigger( 'dragMove' );
-            }
-
-            function dragEnd( e ){
-
-                e.preventDefault();
-
-                that.trigger( 'dragEnd' );
-            }
-
-
         });
+
+        function dragStart( e ) {
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            if ( ! e.touches.length ) return;
+
+            var touch = e.touches[0],
+                webkitTransform = that[ 0 ].style.webkitTransform,
+                lastArr = translateEx.exec( webkitTransform );
+
+            if( lastArr ){
+                lastArr = lastArr[ 1 ].split( ',' );
+            }
+            else {
+                lastArr = [ 0, 0 ];
+            }
+
+            startX = touch.pageX;
+            startY = touch.pageY;
+
+            lastX = parseInt( lastArr[ 0 ] );
+            lastY = parseInt( lastArr[ 1 ] );
+
+            that.trigger( 'dragStart' );
+        }
+
+        function dragMove( e ){
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            if ( !e.touches.length) return;
+
+            var touch = e.touches[0];
+                webkitTransform = that[ 0 ].style.webkitTransform,
+                translateStr = 'translate(';
+
+            curX = touch.pageX - startX + lastX;
+            curY = touch.pageY - startY + lastY;
+
+            if( dragDir === 'xy' ){
+
+                translateStr += curX + 'px, ' + curY + 'px)';
+            }
+            else if( dragDir === 'x' ){
+
+                translateStr += curX + 'px, 0px )';
+            }
+            else {
+
+                translateStr += '0px, ' + curY + 'px)';
+            }
+
+            if( translateEx.test( webkitTransform ) ){
+
+                that[ 0 ].style.webkitTransform = webkitTransform.replace( translateEx, translateStr );
+            }
+            else {
+
+                that[ 0 ].style.webkitTransform += translateStr;
+            }
+
+            that.trigger( 'dragMove' );
+        }
+
+        function dragEnd( e ){
+
+            e.preventDefault();
+            e.stopPropagation();
+
+            that.trigger( 'dragEnd' );
+        }
 
     return this;
 };
