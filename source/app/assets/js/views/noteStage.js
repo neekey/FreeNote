fa3b426c0bcc570727ee616687d9fe0abb9e6425
neekey <ni184775761@gmail.com>
2011-11-02 19:41:01 +0800

    添加保存后滚动到被编辑笔记至屏幕中央

--- source/app/assets/js/views/noteStage.js
+++ source/app/assets/js/views/noteStage.js
@@ -4,7 +4,8 @@
 (function( APP ){
 
 var MODS = APP.mods,
-    VIEWS = APP.views;
+    VIEWS = APP.views,
+    SCREEN = MODS.screen;
 
 var VnoteStage = Backbone.View.extend({
 
@@ -64,6 +65,46 @@ var VnoteStage = Backbone.View.extend({
         var model = this.model.toJSON();
 
         this.el.transform( 'set', { translateX: model.x, translateY: model.y } );
+    },
+
+    /**
+     * 设置noteStage的位置偏移量
+     * @param x
+     * @param y
+     */
+    scrollTo: function( x, y ){
+        /**
+         * @Todo 解决动画scroll出现空白的bug
+         */
+        /*
+        this.el.transform( 'anim', { translateX: trans.x, translateY: trans.y }, 0.5, 'linear', function(){
+
+            var trans = that.el.transform( 'get', 'translate' ),
+                scale = that.el.transform( 'get', 'scale' ),
+                rotate = that.el.transform( 'get', 'rotate' );
+        });
+        */
+
+        this.el.transform( 'set', { translateX: x, translateY: y } );
+        this.render();
+
+    },
+
+    /**
+     * 滚动stage使得指定note处于屏幕中央
+     * @param m
+     */
+    scrollToNote: function( m ){
+
+        var model = m.toJSON(),
+            trans = this.el.transform( 'get', 'translate' ),
+            screenInfo = SCREEN.info,
+            dest = {
+                x: parseInt( - model.x - trans.x + screenInfo.width / 3 ),
+                y: parseInt( - model.y - trans.y + screenInfo.height / 4 )
+            };
+
+        this.scrollTo( dest.x + trans.x, dest.y + trans.y );
     }
 });
 
