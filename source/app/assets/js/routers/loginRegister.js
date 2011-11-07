930c43a1c3db1c429d61d0b5c19d84ce5ad4af79
neekey <ni184775761@gmail.com>
2011-11-07 22:07:01 +0800

    router for login & register

---
+++ source/app/assets/js/routers/loginRegister.js
@@ -0,0 +1,62 @@
+/**
+ * router for login & register
+ */
+
+(function( APP ){
+
+var ROUTERS = APP.routers,
+    VIEWS = APP.views;
+
+var router = Backbone.Router.extend({
+
+    routes: {
+        "": "login",
+        ":type": "handle"
+    },
+
+    initialize: function(){
+
+
+        this.login = new VIEWS[ 'login' ]({
+            el: $( '#J_login-panel' )[ 0 ]
+        });
+
+        this.register = new VIEWS[ 'register' ]({
+            el: $( '#J_register-panel' )[ 0 ]
+        });
+
+        Backbone.history.start();
+
+        this.login.show();
+
+    },
+
+    handle: function( type ){
+        
+        if( type === 'register' ){
+
+            this.registerPage();
+        }
+        else {
+
+            this.loginPage();
+        }
+    },
+
+    loginPage: function(){
+
+        this.register.hide();
+        this.login.show();
+
+    },
+
+    registerPage: function(){
+
+        this.login.hide();
+        this.register.show();
+    }
+});
+
+ROUTERS[ 'loginRegister' ] = router;
+
+})( window[ 'freenote' ] );
