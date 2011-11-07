c81e748321d11a58001eaeea5c1570b22cc27650
neekey <ni184775761@gmail.com>
2011-11-07 14:21:46 +0800

    解决第一次不会自动下滑的bug

--- source/app/assets/js/views/toolMenu.js
+++ source/app/assets/js/views/toolMenu.js
@@ -14,6 +14,7 @@ var VtoolMenu = Backbone.View.extend({
 
         this.el = $( '#J_tool-con' );
         this.handler = $( '#J_toggle-handle' );
+        this.ifFold = true;
 
         // 设置拖拽
         this.el.drag({
