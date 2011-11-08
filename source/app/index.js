24c23dd07b284ad848caaaefd2f6d3726337ce81
neekey <ni184775761@gmail.com>
2011-11-08 11:36:57 +0800

    update

--- source/app/index.js
+++ source/app/index.js
@@ -20,13 +20,12 @@ Rh.init( app );
 app.get( '/*.(js|css)', function( req, res ){
 
     ifFirebug = req.url.substring( 1, 13 );
-    console.log( ifFirebug );
+
     if( ifFirebug !== 'firebug-lite' ){
         var type = 'js';
         if( req.url.substring( req.url.length - 3 ) == 'css' ){
             type = 'css';
         }
-        console.log( "./assets/" + type + req.url );
         res.sendfile( "./assets/" + type + req.url );
     }
     else {
@@ -46,7 +45,7 @@ app.get( '/*.(jpg|gif|png)', function( req, res ){
 
 
     ifFirebug = req.url.substring( 1, 13 );
-    console.log( ifFirebug );
+
     if( ifFirebug !== 'firebug-lite' ){
         
         res.sendfile( './assets/img' + req.url );
@@ -70,59 +69,6 @@ app.get( '/', function( req, res ){
     }); */
     res.render( 'login.jade', {
         layout: false
-    });
-});
-
-/* CRUD router */
-
-// read
-app.get( '/res/note/:id', function( req, res ){
-    var id = req.params.id;
-    console.log( 'id: ' + id );
-    noteHandle.read( id, function( results ){
-        if( results.length ){
-            res.send( results[ 0 ] );
-        }
-        else {
-            res.send( 404 );
-        }
-    });
-});
-
-// create
-app.post( '/res/note/', function( req, res ){
-    var data = req.body;
-    noteHandle.add( data.note, data.author, function( results ){
-        res.send( { id: results.insertId } );
-    });
-});
-// create user
-app.post( '/res/user/', function( req, res ){
-    var data = req.body;
-    Mh.addUser( data.name, data.password, function( err, user ){
-        if( err ){
-            res.send( 404, err );
-        }
-        else {
-            res.send({ _id: user._id });
-        }
-    });
-});
-
-// update
-app.put( '/res/note/:id', function( req, res ){
-    var id = req.params.id,
-        data = req.body;
-    noteHandle.update( id, data, function( results ){
-            res.send({});
-        });
-
-});
-// delete
-app.del( '/res/note/:id', function( req, res ){
-    var id = req.params.id;
-    noteHandle.del( id, function(){
-        res.send({result: true});
     });
 });
 
