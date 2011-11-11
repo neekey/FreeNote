3c313bd581c4c8513a6a9c61399921d8ddfd3a81
neekey <ni184775761@gmail.com>
2011-11-11 22:54:31 +0800

    添加getAllNotes返回指定用户的所有笔记

--- source/app/modules/mongoNoteHandle.js
+++ source/app/modules/mongoNoteHandle.js
@@ -3,17 +3,53 @@
  */
 
 var Mgo = require( './mongoModel' ),
-	Muser = Mgo.model( 'user' ),
+    Muser = Mgo.model( 'user' ),
     errorConf = _freenote_cfg.error;
 
 var handle = {
 
-	/**
-	 * mongoose 2.3.0 的一个bug，一个medel实例对象, 对一个embeded document只能save一次
-	 * @see https://github.com/LearnBoost/mongoose/issues/261
-	 * 因此下面的addNote、delNote，updateNote方法在使用时注意
-	 * 不要使用已经进行过save的user实例对象作为第一个参数
-	 */
+    /**
+     * 返回用户的所有笔记
+     * @param username
+     * @param next( err, notes )
+     *      err: mongo_err | user_not_exist
+     */
+    getAllNotes: function( username, next ){
+
+        var that = this;
+
+        Muser.findOne( { name: username }, function( err, user ){
+
+            if( err ){
+
+                next({
+                    type: 'mongo_error',
+                    msg: errorConf.get( 'mongo_error', err )
+                });
+            }
+            else {
+
+                if( user ){
+
+                    next( null, user.notes );
+                }
+                else {
+
+                    next({
+                        type: 'user_not_exist',
+                        msg: errorConf.get( 'user_not_exist', { name: username } )
+                    });
+                }
+            }
+
+        });
+    },
+    /**
+     * mongoose 2.3.0 的一个bug，一个medel实例对象, 对一个embeded document只能save一次
+     * @see https://github.com/LearnBoost/mongoose/issues/261
+     * 因此下面的addNote、delNote，updateNote方法在使用时注意
+     * 不要使用已经进行过save的user实例对象作为第一个参数
+     */
 
     /**
      * 添加笔记
@@ -22,7 +58,7 @@ var handle = {
      * @param next( err, id )
      *      err: mongo_error | user_not_exist
      */
-	add: function( username, note, next ){
+    add: function( username, note, next ){
 
         var that = this;
 
@@ -57,7 +93,7 @@ var handle = {
                         else {
 
                             that.updateTagNote( oldTags, newTags, tagObj, user, id, function( err ){
-                                
+
                                 if( err ){
 
                                     next( err );
@@ -80,13 +116,13 @@ var handle = {
             }
 
         });
-	},
+    },
 
-	/**
-	 * mongoDB 不允许一次对同一路劲下的数据进行不同类型的操作
-	 * 在修改笔记的操作中，涉及到添加新的tag和修改旧的tag的操作
-	 * 因此这边分成两部，先save，再执行下一步
-	 */
+    /**
+     * mongoDB 不允许一次对同一路劲下的数据进行不同类型的操作
+     * 在修改笔记的操作中，涉及到添加新的tag和修改旧的tag的操作
+     * 因此这边分成两部，先save，再执行下一步
+     */
 
     /**
      * 更新笔记
@@ -96,7 +132,7 @@ var handle = {
      * @param next( err )
      *      err: mongo_error | note_not_exist | user_not_exist
      */
-	update: function( username, id, note, next ){
+    update: function( username, id, note, next ){
 
         var that = this;
 
@@ -152,7 +188,7 @@ var handle = {
             }
         });
 
-	},
+    },
 
     /**
      * 删除笔记
@@ -161,7 +197,7 @@ var handle = {
      * @param next( err )
      *      err: mongo_error | note_not_exist | user_not_exist
      */
-	del: function( username, id, next ){
+    del: function( username, id, next ){
 
         Muser.findOne( { name: username }, function( err, user ){
 
@@ -214,7 +250,7 @@ var handle = {
                 }
             }
         });
-	},
+    },
 
     /**
      * 根据笔记tags信息的更新，更新相关信息
